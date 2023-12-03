41e7719a90c2ddabf8529bd92594ddf80ff80293
Anryurev <137984074+Anryurev@users.noreply.github.com>
2023-12-03 17:26:42 +0700

    Исправил compileSDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.bignerdranch.android.lawquiz"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.bignerdranch.android.lawquiz"
         minSdk = 28
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
