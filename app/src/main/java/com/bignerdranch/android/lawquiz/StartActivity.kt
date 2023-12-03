51d716dc4d44244f83ab7d4476fa3fc78b4736e6
Anryurev <137984074+Anryurev@users.noreply.github.com>
2023-12-03 19:15:23 +0700

    Примерный макет начального экрана приложения и дизайн. Ревализовал переход на основную активность

---
+++ app/src/main/java/com/bignerdranch/android/lawquiz/StartActivity.kt
@@ -0,0 +1,24 @@
+package com.bignerdranch.android.lawquiz
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class StartActivity : AppCompatActivity() {
+    private lateinit var playButton: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+
+        playButton = findViewById(R.id.play_button)
+
+        playButton.setOnClickListener(){
+            val intent = Intent(this,
+                MainActivity::class.java)
+            startActivity(intent)
+
+        }
+    }
+}
